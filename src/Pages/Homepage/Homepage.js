b14d7dc84d407fc71b3a253d17a3cea5a69ffcae
Meeting Room <anas@selteq.net>
2024-07-22 10:42:39 +0500

    updated invoice module design

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -7,10 +7,10 @@ function Homepage() {
     return (
         <div className='col-md-12 home_page_background'>
             <HeaderHome />
-            <div className='col-md-12 py-4'>
+            <div className='col-md-12 padding_screen_top'>
                 <div className='row'>
                     <div className='col-md-6 m-auto padding_left_homepgae padding_top_mobile'>
-                        <p className='m-0 p-0 ps-1'>_Hi all. I am</p>
+                        <p className='m-0 p-0 color_media ps-1'>_Hi all. I am</p>
                         <p className='m-0 p-0 text_fontname'>Anas Waqar</p>
                         <p className='m-0 p-0 text_fontname_career display_pc'>
                             <Typical
@@ -46,7 +46,7 @@ function Homepage() {
                         <p className='m-0 p-0  text_fontname_career_github'><span className='const_text'>const</span> <span className='link_text'>LinkedIn</span> = <span className='link_text_site'>“https://www.linkedin.com/in/anas-waqar-064baa204/”</span></p>
                     </div>
                     <div className='col-md-6 py-5 display_pc'>
-                        <img className='img-fluid' src='/images/snakegame.png' />
+                        <img className='img-fluid img_width_med_screen' src='/images/snakegame.png' />
                     </div>
                 </div>
             </div>
